7283021bbb525adead678ad57433a4c94d06ff56
Jorge L Avila G <jorgelavilag@Jorge-L-Avila-Gs-MacBook-Pro.local>
2018-06-24 15:54:00 -0500

    setting a gulp file for heroku

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('serve', ['sass'], function() {
 //    });
 
     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+//    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 // Move Fonts Folder to src/fonts
