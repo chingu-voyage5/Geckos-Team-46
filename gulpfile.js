a2e8e94216a884e55cae6dfc5389099e45739251
Jorge L Avila G <jorgelavilag@Jorge-L-Avila-Gs-MacBook-Pro.local>
2018-06-24 16:10:37 -0500

    setting a gulp file for heroku

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,14 @@ const sass = require('gulp-sass');
 gulp.task('sass', function() {
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
         .pipe(sass())
-        .pipe(gulp.dest("src/css"));
+        .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
 // Move JS Files to src/js
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest("src/js"));
+        .pipe(gulp.dest("src/js"))
         .pipe(browserSync.stream());
 });
 
