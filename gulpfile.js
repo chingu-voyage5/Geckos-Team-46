fbc1bf94e33c051410f1fd56044520afe5bc07a8
Jorge L Avila G <jorgelavilag@Jorge-L-Avila-Gs-MacBook-Pro.local>
2018-06-24 16:03:28 -0500

    setting a gulp file for heroku

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-//const browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 
 // Compile Sass & Inject Into Browser
@@ -7,24 +7,24 @@ gulp.task('sass', function() {
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
         .pipe(sass())
         .pipe(gulp.dest("src/css"));
-//        .pipe(browserSync.stream());
+        .pipe(browserSync.stream());
 });
 
 // Move JS Files to src/js
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
         .pipe(gulp.dest("src/js"));
-//        .pipe(browserSync.stream());
+        .pipe(browserSync.stream());
 });
 
 // Watch Sass & Server
 gulp.task('serve', ['sass'], function() {
-//    browserSync.init({
-//        server: "./src"
-//    });
+    browserSync.init({
+        server: "./src"
+    });
 
     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-//    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 // Move Fonts Folder to src/fonts
