f0c405972a183010d7d38b99369baf2bb97f8ef0
Jorge L Ávila <jlavil_194@hotmail.com>
2018-06-24 14:13:42 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -39,12 +39,5 @@ gulp.task('fa', function() {
         .pipe(gulp.dest("src/css"));
 });
 
-gulp.task('serveprod', function() {
-  connect.server({
-    root: geckos-team-46.herokuapp.com,
-    port: process.env.PORT || 5000, // localhost:5000
-    livereload: false
-  });
-});
 
-gulp.task('serveprod', ['js', 'serve', 'fa', 'fonts']);
+gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
