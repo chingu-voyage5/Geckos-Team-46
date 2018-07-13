7af2baf37c84ff37e8562f934d84e5dc490fe37c
Jorge L Avila G <jorgelavilag@Jorge-L-Avila-Gs-MacBook-Pro.local>
2018-07-12 22:11:32 -0500

    trying to get the env variables with gulp and setting in the js

--- js/eventbrite.js
+++ js/eventbrite.js
@@ -1,7 +1,7 @@
 class EventBrite {
     // Constructor when instantiate
     constructor() {
-        this.auth_token = process.env.envKey;
+        this.auth_token = window.process.env.envKey;
         this.orderby = 'date';
     }
 
