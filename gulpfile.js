95784c005ccc08d00dcf9fa4fcd49b2f18ee9f4e
Jorge L Avila G <jorgelavilag@Jorge-L-Avila-Gs-MacBook-Pro.local>
2018-07-12 23:49:10 -0500

    trying to get the env variables with gulp and setting in the js

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,6 @@ gulp.task('js', function() {
 // Watch Sass & Server
 gulp.task('serve', ['sass'], function() {
     browserSync.init({
-        window.myKey: process.env.EVENTKEY,
         port: process.env.PORT,
         host: '0.0.0.0',
         notify: false,
